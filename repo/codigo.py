07d5a0abcf44b6419eb21f6daac89efbd38571bc
Eidrean <reinadoadrian3@gmail.com>
2025-11-26 12:29:28 -0500

    cambio comiteado

--- repo/codigo.py
+++ repo/codigo.py
@@ -1,9 +1,5 @@
 nombre= "soy"
-<<<<<<< HEAD
-apellido= "adrian"
-=======
 apellido= "adrian reinado"
->>>>>>> 894c1558d176fca67fbe16935410f6ddd4fcb465
 saludo= nombre + " " + apellido +"como estás"
 
 print(saludo)
